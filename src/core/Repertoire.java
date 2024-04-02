1e81e260ebc145163864b12848e1d83536786139
Ntye <ntye.nina@gmail.com>
2024-04-02 21:57:16 +0200

    add

--- src/core/Repertoire.java
+++ src/core/Repertoire.java
@@ -9,7 +9,10 @@ import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 
 /**
@@ -82,132 +85,102 @@ public class Repertoire {
         }
     }
     
-    public void updateContact(Contact pers) throws ParseException{
-        Scanner sc = new Scanner(System.in);
-        Integer modify;
-        System.out.println("Que voulez-vous modifier à propos de ce contact ?");
-        System.out.println("Le code ? (0)");
-        System.out.println("Le nom ? (1)");
-        System.out.println("La date de naissance ? (2)");
-        System.out.println("L'adresse ? (3)");
-        System.out.println("L'email ? (4)");
-        System.out.println("Le numéro de téléphone ? (5)");
-        System.out.println("Le cycle ? (6)");
-        System.out.println("Le niveau ? (7)");
-        System.out.println("Le statut ? (8)");
-        System.out.println("Le salaire ? (9)");
-        System.out.println("La catégorie ? (10)");
-        System.out.println("L'indice de salaire ? (11)");
-        System.out.println("L'occupation ? (12)");
-        modify = sc.nextInt();
-        if (modify == 0){
-            String change;
-            System.out.println("Veuillez entrer le nouveau code");
-            sc.nextLine();
-            change = sc.nextLine();
-            pers.setCode(change);
-        }
-        if (modify == 1) {
-            String change;
-            System.out.println("Veuillez entrer le nouveau nom");
-            sc.nextLine();
-            change = sc.nextLine();
-            pers.setNom(change);
-        }
-        if (modify == 2) {
-            String change;
-            System.out.println("Veuillez entrer la nouvelle date de naissance au format (dd/MM/yyyy)");
-            sc.nextLine();
-            change = sc.nextLine();
-            Date date = new SimpleDateFormat("dd/MM/yyyy").parse(change);
-            pers.setDateDeNaissance(date);
-        }
-        if (modify == 3){
-            String change;
-            System.out.println("Veuillez entrer la nouvelle adresse");
-            sc.nextLine();
-            change = sc.nextLine();
-            pers.setAddress(change);
-        }
-        if (modify == 4){
-            String change;
-            System.out.println("Veuillez entrer la nouvelle adresse email");
-            sc.nextLine();
-            change = sc.nextLine();
-            pers.setEmail(change);
-        }
-        if (modify == 5){
-            String change;
-            System.out.println("Veuillez entrer le nouveau numéro de téléphone");
-            sc.nextLine();
-            change = sc.nextLine();
-            pers.setTelNumber(change);
-        }
-        if (modify == 6){
-            String change;
-            System.out.println("Veuillez entrer le nouveau cycle");
-            sc.nextLine();
-            change = sc.nextLine();
-            Etudiant e = (Etudiant)pers;
-            e.setCycle(change);
-        }
-        if (modify == 7){
-            int change;
-            System.out.println("Veuillez entrer le nouveau niveau");
-            sc.nextLine();
-            change = sc.nextInt();
-            Etudiant e = (Etudiant)pers;
-            e.setNiveau(change);
-        }
-        if (modify == 8){
-            String change;
-            Integer choix;
-            System.out.println("S'agit-il d'un enseignant(0) ou d'un agent(1) ?");
-            choix = sc.nextInt();
-            if (choix == 0){
-                System.out.println("Veuillez entrer le nouveau statut");
-                sc.nextLine();
-                change = sc.nextLine();
-                Enseignant e = (Enseignant)pers;
-                e.setStatut(change);
-            }else {
-                System.out.println("Veuillez entrer le nouveau statut");
-                sc.nextLine();
-                change = sc.nextLine();
-                Agent a = (Agent)pers;
-                a.setStatut(change);
+    public void updateContact(Contact pers, int modify, String change) throws ParseException{
+        switch (modify) {
+            case 1 -> pers.setCode(change);
+            case 2 -> pers.setNom(change);
+            case 3 -> {
+                Date date = new SimpleDateFormat("dd/MM/yyyy").parse(change);
+                pers.setDoB(date);
             }
+            case 4 -> pers.setAddress(change);
+            case 5 -> pers.setEmail(change);
+            case 6 -> pers.setTelNum(change);
+//            case 7:
+//                pers.setCycle(change);
+//                break;
+//            case 7:
+//                pers.setNiveau(change);
+//                break;
+//            case 7:
+//                pers.setCycle(change);
+//                break;   
+//            case 7:
+//                pers.setNiveau(change);
+//                break;  
+                
         }
-        if (modify == 9){
-            Integer change;
-            System.out.println("Veuillez entrer le nouveau salaire");
-            Agent a = (Agent)pers;
-            change = sc.nextInt();
-            a.setSalaire(change);
-        }
-        if (modify == 10){
-            String change;
-            System.out.println("Veuillez entrer la nouvelle catégorie");
-            Agent a = (Agent)pers;
-            sc.nextLine();
-            change = sc.nextLine();
-            a.setCategorie(change);
-        }
-        if (modify == 11){
-            Integer change;
-            System.out.println("Veuillez entrer le nouvel indice du salaire");
-            Agent a = (Agent)pers;
-            change = sc.nextInt();
-            a.setIndiceSalaire(change);
-        }
-        if (modify == 12){
-            String change;
-            System.out.println("Veuillez entrer la nouvelle occupation");
-            Agent a = (Agent)pers;
-            sc.nextLine();
-            change = sc.nextLine();
-            a.setOccupation(change);
-        }
-         
     }
+    
+    public void updateEtudiant(Etudiant pers, int modify, String change) throws ParseException{
+        if(modify < 7) {
+            updateContact(pers, modify, change);
+            return;
+        }
+        else{
+            switch (modify) {
+            case 1 -> pers.setCycle(change);
+            case 2 -> pers.setNiveau(change);
+            }    
+        }
+    }
+//        if (modify == 7){
+//            int change;
+//            System.out.println("Veuillez entrer le nouveau niveau");
+//            sc.nextLine();
+//            change = sc.nextInt();
+//            Etudiant e = (Etudiant)pers;
+//            e.setNiveau(change);
+//        }
+//        if (modify == 8){
+//            String change;
+//            Integer choix;
+//            System.out.println("S'agit-il d'un enseignant(0) ou d'un agent(1) ?");
+//            choix = sc.nextInt();
+//            if (choix == 0){
+//                System.out.println("Veuillez entrer le nouveau statut");
+//                sc.nextLine();
+//                change = sc.nextLine();
+//                Enseignant e = (Enseignant)pers;
+//                e.setStatut(change);
+//            }else {
+//                System.out.println("Veuillez entrer le nouveau statut");
+//                sc.nextLine();
+//                change = sc.nextLine();
+//                Agent a = (Agent)pers;
+//                a.setStatut(change);
+//            }
+//        }
+//        if (modify == 9){
+//            Integer change;
+//            System.out.println("Veuillez entrer le nouveau salaire");
+//            Agent a = (Agent)pers;
+//            change = sc.nextInt();
+//            a.setSalaire(change);
+//        }
+//        if (modify == 10){
+//            String change;
+//            System.out.println("Veuillez entrer la nouvelle catégorie");
+//            Agent a = (Agent)pers;
+//            sc.nextLine();
+//            change = sc.nextLine();
+//            a.setCategorie(change);
+//        }
+//        if (modify == 11){
+//            Integer change;
+//            System.out.println("Veuillez entrer le nouvel indice du salaire");
+//            Agent a = (Agent)pers;
+//            change = sc.nextInt();
+//            a.setIndiceSalaire(change);
+//        }
+//        if (modify == 12){
+//            String change;
+//            System.out.println("Veuillez entrer la nouvelle occupation");
+//            Agent a = (Agent)pers;
+//            sc.nextLine();
+//            change = sc.nextLine();
+//            a.setOccupation(change);
+//        }
+//         
+//    }
 }
